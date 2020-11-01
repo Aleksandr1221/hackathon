5babfe42e68bbff41325e23c18d97000436c326e
Gerasimov Aleksandr <aleksandr122104@gmail.com>
2020-11-01 15:32:41 +0900

    finalUpdate

--- src/views/vTodo.vue
+++ src/views/vTodo.vue
@@ -1,19 +1,33 @@
 <template>
 <div>
 
-  <div class="row" v-for="(todo, id) in todoList" :key="id">
+  <!--<div class="row" v-for="(todo, id) in todoList" :key="id">
   <div class="col-3 pr-0">
     <div class="list-group rounded-0" id="list-tab" role="tablist">
       <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab">{{todo.name}}</a>
-      <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profile</a>
     </div>
   </div>
   <div class="col-9 border-top">
     <div class="tab-content" id="nav-tabContent">
-      <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">{{todo.desc}}</div>
+      <div class="tab-pane fade show active" id="list-home active" role="tabpanel" aria-labelledby="list-home-list">{{todo.description}}</div>
     </div>
   </div>
-  </div>
+  </div>-->
+
+
+
+   <div class="row row-cols-1 mt-1 row-cols-md-2 g-5 pl-5 w-100" v-if="todoList.length > 0">
+          <div class="col" v-for="(todo, id) in todoList" :key="id">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">{{todo.name}}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Баллов: {{todo.points}}</h6>
+                <p class="card-text">Описание: {{todo.description}}</p>
+                <button class="btn btn-primary">Выполнено</button>
+              </div>
+            </div>
+          </div>
+        </div>
 </div>
 </template>
 
@@ -29,13 +43,14 @@ export default {
   },
   methods: {
     getTodo: function() {
-      axios.get('https://vk-hack-ykt.herokuapp.com/registration/')
+      axios.get('https://vk-hack-ykt.herokuapp.com/task/user/' + localStorage.getItem("localEmail"))
       .then((response) => (
-        console.log(response),
-        response.data.find(element => element.username === localStorage.getItem('localEmail')).tasks.forEach(element => {
-          this.todoList.push({name: element.name, desc: element.desc})
-          console.log(element)
-        })
+        console.log(response.data),
+        this.todoList = response.data
+        //response.data.find(element => element.username === localStorage.getItem('localEmail')).tasks.forEach(element => {
+        //  this.todoList.push({name: element.name, desc: element.desc})
+        //  console.log(element)
+        //}
 
       )).catch(error => (console.log(error)))
     }
